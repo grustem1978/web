32a74eb61f9c420d913fa384c00e322be6d7a863
grustem <grustem@gmail.com>
2020-11-22 17:07:23 +0000

    Try

--- hello.py
+++ hello.py
@@ -8,3 +8,14 @@ def simple_app(environ, start_response):
     response_headers = [('Content-type', 'text/plain')]
     start_response(status, response_headers)
     return 'Hello world!\n'
+
+def app(environ, start_response):
+    """Simplest possible application object"""
+    data = 'Hello, World!\n'
+    status = '200 OK'
+    response_headers = [
+        ('Content-type', 'text/plain'),
+        ('Content-Length', str(len(data)))
+    ]
+    start_response(status, response_headers)
+    return iter([data])
