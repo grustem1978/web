270d0d395593b4140b084141e01656e6b8368988
grustem <grustem@gmail.com>
2020-11-25 11:12:30 +0000

    gunicorn

--- 1.sh
+++ 1.sh
@@ -1,7 +1,8 @@
 sudo rm /etc/nginx/sites-enabled/*
 sudo ln -sf /home/box/web/etc/nginx.conf  /etc/nginx/sites-enabled/test.conf
-
 sudo /etc/init.d/nginx restart
+
+sudo rm /etc/gunicorn.d/*
 sudo ln -sf /home/box/web/etc/gunicorn.conf   /etc/gunicorn.d/test
-sudo /etc/init.d/gunicorn restart /etc/init.d/test
+sudo /etc/init.d/gunicorn restart
 #﻿sudo /etc/init.d/mysql start﻿
--- hello.py
+++ hello.py
@@ -12,12 +12,12 @@ def simple_app(environ, start_response):
 def app(environ, start_response):
     """Simplest possible application object"""
     data = 'Hell'
-    body = [(i + '\n') for i in environ['QUERY_STRING'].split('&')]
-    body = [bytes(i + '\n', 'ascii') for i in environ['QUERY_STRING'].split('&')
+    body = environ['QUERY_STRING'].replace('&','\n')
+    #body = [bytes(i + '\n', 'ascii') for i in environ['QUERY_STRING'].split('&')
     status = '200 OK'
     response_headers = [
         ('Content-type', 'text/plain'),
         ('Content-Length', str(len(body)))
     ]
     start_response(status, response_headers)
-    return body
+    return [body]
