a04b601744dd95b7d23e4cde17828948cf5268f0
grustem <grustem@gmail.com>
2020-12-01 06:52:22 +0000

    dg3

--- 1.sh
+++ 1.sh
@@ -7,3 +7,5 @@ sudo ln -sf /home/box/web/etc/gunicorn.conf   /etc/gunicorn.d/test
 sudo ln -sf /home/box/web/etc/ask.conf /etc/gunicorn.d/ask
 sudo /etc/init.d/gunicorn restart
 #﻿sudo /etc/init.d/mysql start﻿
+git config --global user.email "grustem@gmail.com"
+git config --global user.name "grustem"
