01de5f23640154160a33f53ddb023ba406c0dc2d
grustem <grustem@gmail.com>
2020-12-04 09:47:23 +0000

    MVC_

--- ask/ask/settings.py
+++ ask/ask/settings.py
@@ -23,7 +23,7 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 SECRET_KEY = '&&&3f)c@%9rud2c^tmz-15rrsdr&6zmu14_c2flzz6bxc$p47^'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
 ALLOWED_HOSTS = ['*']
 
