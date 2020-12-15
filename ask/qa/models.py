02112c00518c5b3caa24a8b8716df32e2c284828
grustem <grustem@gmail.com>
2020-12-15 07:01:13 +0000

    mysql

--- ask/qa/models.py
+++ ask/qa/models.py
@@ -5,10 +5,23 @@ from django.db import models
 from django.contrib.auth.models import User
 
 # Create your models here.
+class QuestionManager(models.Manager): 
+  def new(self):
+    return self.order_by('-added_at')  
+  def popular(self): 
+    return self.order_by('-rating')
+
+
+
+
+
+
+
 class Question(models.Model):
+  objects = QuestionManager() 
   title = models.CharField(max_length=255)
   text = models.TextField()
-  added_ad = models.DateTimeField(auto_now_add=True)
+  added_ad = models.DateTimeField(blank = True, auto_now_add=True)
   rating = models.IntegerField(default=0)
   author = models.ForeignKey(User)
   likes = models.ManyToManyField(User, related_name='likes_set')
@@ -17,13 +30,21 @@ class Question(models.Model):
 #  def get_absolute_url(self):
 #    return '/post/%d/' % self.pk
   class Meta:
-    db_table = 'quest_table'
+    db_table = 'questions'
     ordering = ['-added_ad']
 
-class QuestionManager(models.Manager):
-  def new(self):
-    return self.order_by('-added_at')
-  def popular(self):
-    return self.order_by('-rating')
 
 
+class Answer(models.Model): 
+  title = models.CharField(max_length=255)
+  text = models.TextField()
+  added_ad = models.DateTimeField(blank = True, auto_now_add=True)
+  question = models.ForeignKey(Question)
+  author = models.ForeignKey(User)
+  def __unicode__(self):
+    return self.text
+#  def get_absolute_url(self):
+#    return '/post/%d/' % self.pk
+  class Meta:
+    db_table = 'answers'
+    ordering = ['-added_ad']
